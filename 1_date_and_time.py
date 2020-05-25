87452864c828a8414c9c1794b814351678580a78
Evhenii Beznisko <evhenii.beznysko@gmail.com>
2020-05-25 15:08:11 +0300

    Сделал задание с датой и работой с файлом

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -3,17 +3,34 @@
 
 Дата и время
 
-1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
+1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад        +
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
+import locale 
+locale.setlocale(locale.LC_ALL, "russian")
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt_now = datetime.now()
+
+    #вчера
+    delta = timedelta(days=1)
+    result = dt_now - delta
+    print(result.strftime('%A %d %B %Y'))
+
+    #сегодня
+    print(dt_now)
+
+    #30 дней назад
+    result = dt_now - delta
+    print(result.strftime('%A %d %B %Y'))
+
+
 
 
 def str_2_datetime(date_string):
@@ -21,7 +38,10 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+      
+    dt = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
+    return dt
+
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -4,19 +4,32 @@
 Работа с файлами
 
 
-1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0
-2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки
-3. Подсчитайте количество слов в тексте
-4. Замените точки в тексте на восклицательные знаки
-5. Сохраните результат в файл referat2.txt
+1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0   +
+2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки      +
+3. Подсчитайте количество слов в тексте                                                 +
+4. Замените точки в тексте на восклицательные знаки                                     +
+5. Сохраните результат в файл referat2.txt                                              +
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    new_content = ""
+    with open('referat.txt', 'r', encoding='utf-8') as text:
+        content = text.read()
+        print(len(content))
+
+        word_count = content.split(" ")
+        word_count = len(word_count)
+        print(word_count)
+
+        new_content = content.replace('.', '!')
+
+    with open('referat2.txt', 'w', encoding='utf-8') as text:
+        text.write(new_content)
+
+        
+
+
+    
 
 if __name__ == "__main__":
     main()
